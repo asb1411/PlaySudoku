e06ec4ec31c0f264c2411ac4225e1c819148b615
Rahil Jain <f20170541@pilani.bits-pilani.ac.in>
2023-03-03 19:38:27 +0530

    init

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/**
+ * React component for the Footer Section.
+ */
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&#169; 2023 psychorhuk - source code on <a href="https://github.com/psychorhuk/sudoku">Github</a></p>
+    </footer>
+  )
+}
